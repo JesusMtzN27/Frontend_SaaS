2dd49832a7aea87ca223bb64dec0a52cd0e2a67f
JesusMtzN27 <developer42@grupotransforma.mx>
2025-04-25 16:16:24 -0600

    Cuarta Versión

--- src/components/ReportMenu.js
+++ src/components/ReportMenu.js
@@ -38,8 +38,10 @@ const ReportMenu = () => {
         {embedUrl && (
           <iframe
             src={embedUrl}
+            // src="https://us-east-1.quicksight.aws.amazon.com/embed/d8ba45c3bd7b4718be9e21b5c94dddfd/dashboards/09481f25-820f-476b-b351-a3f47b9901e6?identityprovider=quicksight&isauthcode=true&code=AYABeMBkrSGXJYtfQz9huHJSTuYAAAABAAdhd3Mta21zAEthcm46YXdzOmttczp1cy1lYXN0LTE6MjU5NDgwNDYyMTMyOmtleS81NGYwMjdiYy03MDJhLTQxY2YtYmViNS0xNDViOTExNzFkYzMAuAECAQB4g1oL4hdUJZc1aKfcGo-VQb_jBEsh0RowAd9MxoJqXpEBKUVpCWD2t4B-bPZY5kqhAAAAAH4wfAYJKoZIhvcNAQcGoG8wbQIBADBoBgkqhkiG9w0BBwEwHgYJYIZIAWUDBAEuMBEEDC3yEk_PLP7k8t0howIBEIA7HSFqYC1AovylIRwqqLSsLuc7V1OEuwcgTmMwZ-lqN_5qvLDMj1Cb8qAXc2f6OlwAd6bq32OgBZ0WGYgCAAAAAAwAABAAAAAAAAAAAAAAAAAAgRJtdBS1WXapVi4FfhIEPP____8AAAABAAAAAAAAAAAAAAABAAAAmyzt8Q3fmmokGANZl4Qbb62IRMggcPoto6IcFvENSWSU_uGmDWvcEJTkY8ww-e1nrqDO_DEZRb0MK6b-09xmr8X62rqSiXpiiG7pSsje8gb_uYueKCaS8XAmjohk_iYSAIrWOvBVlozJm6DQK8PvUmioYK-TBAPSPlsaEbQvAhshrjetsTK__-_UvEm5S-eKDnkf6fjhuEM37w9w9Pe0ywsG8NY6FdcYiTgZMw%3D%3D" // Cambia esto por la URL de tu reporte
             width="100%"
-            height="600"
+            height="800"
+            frameBorder="0"
             title="Reporte de QuickSight"
           ></iframe>
         )}
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,24 +1,47 @@
 // src/Welcome.js
 import React from 'react';
 import './Welcome.css'; // Importar el archivo CSS
-import ReportMenu from './ReportMenu'; // Importa el componente ReportMenu
+//import ReportMenu from './ReportMenu'; // Importa el componente ReportMenu
 
 const Welcome = () => {
-  const username = localStorage.getItem("username");
+  //const username = localStorage.getItem("username");
 
   return (
     <div className="welcome-container">
-      <div className="report-menu-container">
+      {/* <div className="report-menu-container">
         <ReportMenu />
       </div>
 
       <br />
-      
+
       <div className="welcome-message">
         <h2>Bienvenido, {username}!</h2>
         <p>¡Aquí está tu contenido exclusivo después de iniciar sesión!</p>
         
-      </div>
+      </div> */}
+
+      <br />
+      <br />
+
+      <iframe
+          title="QuickSight Report"
+          width="100%"
+          height="800"
+          //src="https://us-east-1.quicksight.aws.amazon.com/sn/embed/share/accounts/297009612081/dashboards/a5f331d6-acbf-4579-84ff-b7e6ad84593a?directory_alias=Operaria-Quicksight&username=AWSReservedSSO_AdministratorAccess_84fc8e51a827a37b/JesusMartinez"
+          src="https://us-east-1.quicksight.aws.amazon.com/sn/embed/share/accounts/297009612081/dashboards/a5f331d6-acbf-4579-84ff-b7e6ad84593a?directory_alias=Operaria-Quicksight&username=AWSReservedSSO_AdministratorAccess_84fc8e51a827a37b/OmarAnguiano"
+          //src="https://us-east-1.quicksight.aws.amazon.com/sn/embed/share/accounts/297009612081/dashboards/a0ff86e4-dca2-4892-9d2e-551a313af7ee?directory_alias=Operaria-Quicksight&username=backpirelli"
+          frameBorder="0"
+        ></iframe>
+        <br />
+      <iframe
+          title="QuickSight Report"
+          width="100%"
+          height="800"
+          src="https://us-east-1.quicksight.aws.amazon.com/sn/embed/share/accounts/297009612081/dashboards/a5f331d6-acbf-4579-84ff-b7e6ad84593a?directory_alias=Operaria-Quicksight&username=AWSReservedSSO_AdministratorAccess_84fc8e51a827a37b/JesusMartinez"
+          //src="https://us-east-1.quicksight.aws.amazon.com/sn/embed/share/accounts/297009612081/dashboards/a5f331d6-acbf-4579-84ff-b7e6ad84593a?directory_alias=Operaria-Quicksight&username=AWSReservedSSO_AdministratorAccess_84fc8e51a827a37b/OmarAnguiano"
+          //src="https://us-east-1.quicksight.aws.amazon.com/sn/embed/share/accounts/297009612081/dashboards/a0ff86e4-dca2-4892-9d2e-551a313af7ee?directory_alias=Operaria-Quicksight&username=backpirelli"
+          frameBorder="0"
+        ></iframe>
 
     </div>
   );
