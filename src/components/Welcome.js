24909bb559c1f5c94cf2c0d4070390d5ed569d38
JesusMtzN27 <developer42@grupotransforma.mx>
2025-04-30 17:26:00 -0600

    Quinta Versión

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -22,24 +22,12 @@ const Welcome = () => {
 
       <br />
       <br />
-
+      
       <iframe
           title="QuickSight Report"
           width="100%"
-          height="800"
-          //src="https://us-east-1.quicksight.aws.amazon.com/sn/embed/share/accounts/297009612081/dashboards/a5f331d6-acbf-4579-84ff-b7e6ad84593a?directory_alias=Operaria-Quicksight&username=AWSReservedSSO_AdministratorAccess_84fc8e51a827a37b/JesusMartinez"
-          src="https://us-east-1.quicksight.aws.amazon.com/sn/embed/share/accounts/297009612081/dashboards/a5f331d6-acbf-4579-84ff-b7e6ad84593a?directory_alias=Operaria-Quicksight&username=AWSReservedSSO_AdministratorAccess_84fc8e51a827a37b/OmarAnguiano"
-          //src="https://us-east-1.quicksight.aws.amazon.com/sn/embed/share/accounts/297009612081/dashboards/a0ff86e4-dca2-4892-9d2e-551a313af7ee?directory_alias=Operaria-Quicksight&username=backpirelli"
-          frameBorder="0"
-        ></iframe>
-        <br />
-      <iframe
-          title="QuickSight Report"
-          width="100%"
-          height="800"
-          src="https://us-east-1.quicksight.aws.amazon.com/sn/embed/share/accounts/297009612081/dashboards/a5f331d6-acbf-4579-84ff-b7e6ad84593a?directory_alias=Operaria-Quicksight&username=AWSReservedSSO_AdministratorAccess_84fc8e51a827a37b/JesusMartinez"
-          //src="https://us-east-1.quicksight.aws.amazon.com/sn/embed/share/accounts/297009612081/dashboards/a5f331d6-acbf-4579-84ff-b7e6ad84593a?directory_alias=Operaria-Quicksight&username=AWSReservedSSO_AdministratorAccess_84fc8e51a827a37b/OmarAnguiano"
-          //src="https://us-east-1.quicksight.aws.amazon.com/sn/embed/share/accounts/297009612081/dashboards/a0ff86e4-dca2-4892-9d2e-551a313af7ee?directory_alias=Operaria-Quicksight&username=backpirelli"
+          height="1000"
+          src="https://us-east-1.quicksight.aws.amazon.com/sn/embed/share/accounts/297009612081/dashboards/f2f70081-8331-4fb5-918f-000af02298d6?directory_alias=Operaria-Quicksight" // Demography
           frameBorder="0"
         ></iframe>
 
