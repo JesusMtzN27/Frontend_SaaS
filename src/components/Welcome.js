50905873be38a43dc82c89fdf80571261f4463d7
JesusMtzN27 <developer42@grupotransforma.mx>
2025-04-30 17:28:35 -0600

    Quinta Versión

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -27,7 +27,7 @@ const Welcome = () => {
           title="QuickSight Report"
           width="100%"
           height="1000"
-          src="https://us-east-1.quicksight.aws.amazon.com/sn/embed/share/accounts/297009612081/dashboards/f2f70081-8331-4fb5-918f-000af02298d6?directory_alias=Operaria-Quicksight" // Demography
+          src="https://us-east-1.quicksight.aws.amazon.com/embed/f7ddd7b266ec4faebffd7c91355ec6e6/dashboards/f2f70081-8331-4fb5-918f-000af02298d6?identityprovider=quicksight&isauthcode=true&code=AYABeAP8e7OsDXErCo5dYNJO8TYAAAABAAdhd3Mta21zAEthcm46YXdzOmttczp1cy1lYXN0LTE6MjU5NDgwNDYyMTMyOmtleS81NGYwMjdiYy03MDJhLTQxY2YtYmViNS0xNDViOTExNzFkYzMAuAECAQB4g1oL4hdUJZc1aKfcGo-VQb_jBEsh0RowAd9MxoJqXpEBHHW81baC10bDW6At7MTpNwAAAH4wfAYJKoZIhvcNAQcGoG8wbQIBADBoBgkqhkiG9w0BBwEwHgYJYIZIAWUDBAEuMBEEDG0sHoEwr33KwdG38wIBEIA7lOk8W5ttNmLP7NQTcnjNRizofUJXZt29xL0oBRDUbNEmcTBW_f7HAwB_hUY5d90cGvOxNo-Vkwht4ToCAAAAAAwAABAAAAAAAAAAAAAAAAAACrYqq_q9yDB2SzVezo_KQf____8AAAABAAAAAAAAAAAAAAABAAAAm_br3tHchlGdxIVnYExqosDy8bDQrXWTwybNAlA6E8bF5sjoxRL5K5W-5DA_8jUA6do_fD_6211UWedu6hgmoz6R2z1TwoSH20nXn2UbJ5CI23l38OldMq4mcePwAzd19c8SN0xw22fqISEp-KOcsF03lslIvuU85pfsONaWE5_-ffjBfZjAFpeqK-Y36GjvezRZeLQTddc87bpytbHmzPAhnw0q9qm_xhLiAA%3D%3D" // Demography
           frameBorder="0"
         ></iframe>
 
